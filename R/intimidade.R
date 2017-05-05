d6325e80635e618f596eff1f10b6ea79375f5126
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2017-05-05 15:08:15 -0300

    Conta o que pra contar

--- R/intimidade.R
+++ R/intimidade.R
@@ -1,13 +1,13 @@
-get_macs_perenes <- function(macs_vistos) {
+get_macs_perenes <- function(macs_macs_vistos) {
     library(dplyr)
     library(lubridate)
     
-    dias_possiveis = macs_vistos %>% 
+    dias_possiveis = macs_macs_vistos %>% 
         transmute(dia = paste(year(t), yday(t))) %>% 
         distinct() %>% 
         NROW()
     
-    sempre_la = macs_vistos %>% 
+    sempre_la = macs_macs_vistos %>% 
         filter(hour(t) %in% 1:6) %>% 
         group_by(mac, dia = day(t)) %>% 
         summarise(n = n()) %>% 
@@ -16,4 +16,15 @@ get_macs_perenes <- function(macs_vistos) {
         filter(dias >= .2) # decidimos .2 olhando o histograma
     
     return(sempre_la)
+}
+
+conta_macs <- function(macs_vistos, intervalo = 5) {
+    library(dplyr)
+    library(lubridate)
+    macs_vistos %>% 
+        mutate(dia = floor_date(t, unit = "day")) %>% 
+        group_by(periodo = floor_date(t, unit = "hour") + floor(minute(t)/intervalo)*intervalo) %>%
+        distinct(periodo, mac, .keep_all = TRUE) %>% 
+        count() %>% 
+        return()
 }
